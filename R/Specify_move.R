3ac56dae851f33c450a8e171e1d956c165132564
Chengxue Li <chengxue.li@nmfs.local>
2024-08-07 11:13:25 -0400

    Updated R code

--- R/Specify_move.R
+++ R/Specify_move.R
@@ -3,7 +3,7 @@
 #' This function launches the Shiny app included in the package.
 #' @export
 Specify_move <- function() {
-  appDir <- system.file("shiny/myapp", package = "whamMSE")
+  appDir <- system.file("shinyApp/app.R", package = "whamMSE")
   if (appDir == "") {
     stop("Could not find the Shiny app directory. Try re-installing `whamMSE`.", call. = FALSE)
   }
